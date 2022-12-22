009c5d5c4575b497a5343de47a9b6defa229f060
Andre Flor <72424051+andreflor21@users.noreply.github.com>
2022-12-21 23:38:32 -0300

    using ip on api baseurl

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://0.0.0.0:3333/',
-});
 // const api = axios.create({
-//   baseURL: 'http://3.83.86.36:3333/',
-// });
+//  baseURL: 'http://0.0.0.0:3333/',
+//});
+ const api = axios.create({
+   baseURL: 'http://3.83.86.36:3333/',
+ });
 
 export default api;
