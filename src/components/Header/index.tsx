e431ceed3e2cfb5737e4345bc86004b93e9db3ed
Andre Flor <andreflor96@hotmail.com>
2023-03-13 22:44:20 -0300

    add Menu compontent

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,19 @@
 import React from 'react';
 import logo from '../../assets/logo_haizer.png';
+import { Menu } from '../Menu';
 // import { Container } from './styles';
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-around h-28 fixed bg-[#fff] z-20 w-screen top-0">
-      <a href="https://www.haizerusa.com" className="w-1/2">
-        <img src={logo} className="w-[300px] self-start" />
+    <header className="flex items-center justify-between h-28 fixed bg-[#fff] z-20 w-screen top-0">
+      <a href="https://www.haizerusa.com" className="relative w-1/2">
+        <img src={logo} className="max-w-[208px] xs:max-w-[300px]" />
       </a>
-      <div className="flex items-center justify-between w-1/5 max-w-1/2">
-        <a
-          className="tracking-[3.5px] font-bold text-zinc-800 text-sm  hover:text-orange-500 hover:underline"
-          href="https://www.haizerusa.com/be-a-dealer"
-          target="_blank"
-        >
-          BE A DEALER
-        </a>
-        <a
-          className="tracking-[3.5px] font-bold text-zinc-800 text-sm  hover:text-orange-500 hover:underline"
-          href="https://www.haizerusa.com/be-a-dealer"
-          target="_blank"
-        >
-          CONTACT US
-        </a>
-      </div>
+      {window.innerWidth < 640 ? (
+        <Menu display="mobile" />
+      ) : (
+        <Menu display="normal" />
+      )}
     </header>
   );
 };
