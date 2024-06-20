253153b4d92486cfe77355c48170f5c75cfaf49d
Andre Flor <72424051+andreflor21@users.noreply.github.com>
2024-06-19 23:19:52 -0300

    Update api.tsx
    To production URL

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const api = axios.create({
-    baseURL: 'http://localhost:3333/',
-});
 // const api = axios.create({
-//     baseURL: 'https://bulb-finder.haizerusa.com:3333/',
+//     baseURL: 'http://localhost:3333/',
 // });
+const api = axios.create({
+    baseURL: 'https://bulb-finder-api.vercel.app/',
+});
 
 export default api;
